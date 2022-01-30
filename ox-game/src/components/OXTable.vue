fbe8cc800cc7c78d2c150a5e4686e940ff1b7937
Jakkrit Chaopron <idontknowbrotherpls@gmail.com>
2022-01-30 23:06:08 +0700

    done: using v-for added - to OXTable

--- ox-game/src/components/OXTable.vue
+++ ox-game/src/components/OXTable.vue
@@ -1,26 +1,24 @@
 <template>
     <div>
         <table>
-            <tr>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-            </tr>
-            <tr>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
-            </tr>
-            <tr>
-                <td>-</td>
-                <td>-</td>
-                <td>-</td>
+            <tr v-for="(line, lineId) in table" :key="lineId">
+                <td v-for="(item, itemId) in line" :key="itemId">{{item}}</td>
             </tr>
         </table>
     </div>
 </template>
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      table: [
+        ['-', '-', '-'],
+        ['-', '-', '-'],
+        ['-', '-', '-']
+      ]
+    }
+  }
+}
 </script>
 <style>
 </style>
