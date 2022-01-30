4b97046b3eb8b4eba97c6953f79cf7ed6cda2fb1
Jakkrit Chaopron <idontknowbrotherpls@gmail.com>
2022-01-30 21:22:39 +0700

    added: myImg.gif to MyPage inde, added: MyGame Page

--- ox-game/src/App.vue
+++ ox-game/src/App.vue
@@ -3,6 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/mypage/Jakkrit">MyPage</router-link> |
+    <router-link to="/mygame/">MyGame</router-link>
   </div>
   <router-view/>
 </template>
--- ox-game/src/views/MyPage.vue
+++ ox-game/src/views/MyPage.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>MyPage</h1>
     <h3> {{ name }}</h3>
+    <img src="../assets/myImg.gif" alt="">
   </div>
 </template>
 <script>
